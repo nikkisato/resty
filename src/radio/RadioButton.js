bffee6e8220b9f0c910d13ab51958afa1c2d4cb5
Nikki <nikki.satopdx@gmail.com>
2020-01-31 14:32:46 -0800

    creating radio Buttons

---
+++ src/radio/RadioButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './RadioButton.css';
+
+const RadioButton = ({ name, label, value, onChange }) => (
+  <div className={styles.RadioButton}>
+    <input id={styles.RadioButton} type name={name} value={value} onChange={onChange}/>
+    <label htmlFor={value}>{label}</label>
+
+  </div>
+);
+
+RadioButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+   
+};
+
+
+export default RadioButton;
